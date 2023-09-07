c148f65ccf8080de1476837a4cf890d65e164a22
kartik126 <lavi9921@gmail.com>
2023-09-07 21:45:29 +0530

    Setup:home page layout

---
+++ src/Layouts/Home.tsx
@@ -0,0 +1,15 @@
+import Footer from "@/components/modules/Footer";
+import Header from "@/components/modules/Header";
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <p>home page layout.......</p>
+      <Footer/>
+    </>
+  );
+}
+
+export default Home;
