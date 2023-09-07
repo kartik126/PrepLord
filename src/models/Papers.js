f2082ba8c11414f3deba7fd68604ee18b875434f
kartik126 <lavi9921@gmail.com>
2023-09-07 13:30:43 +0530

    Api:past papers end point

---
+++ src/models/Papers.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const papersSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now,
+  },
+  data: {
+    type: Buffer, // Use the Buffer data type for binary data
+    required: true,
+  },
+});
+
+const fileSchema = new mongoose.Schema({
+  exam_name: {
+    type: String,
+    required: true,
+  },
+  papers: [papersSchema],
+});
+
+const Papers = mongoose.model.papers || mongoose.model("papers", fileSchema);
+
+export default Papers;
