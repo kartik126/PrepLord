76d9bc179e4395fa264a13196998461283bef0ea
kartik126 <lavi9921@gmail.com>
2023-09-19 23:33:09 +0530

    UI:signup modal component

--- src/components/layouts/Signup.tsx
+++ src/components/layouts/Signup.tsx
@@ -1,11 +1,9 @@
 import { primary_color } from "@/utils/Colors";
 import React, { useState } from "react";
-import Signup from "./Signup";
 
-function Login({ open, setopen }: any) {
+function Signup({ open, setopen }: any) {
   console.log(open);
   const [isModalOpen, setIsModalOpen] = useState(true);
-  const [signupOpen, setsignupOpen] = useState(false);
 
   const closeModal = () => {
     // setIsModalOpen(false);
@@ -14,7 +12,6 @@ function Login({ open, setopen }: any) {
 
   return (
     <>
-      <Signup open={signupOpen} setopen={setsignupOpen} />
       <div
         className={`fixed top-0 left-0 flex items-center justify-center w-full h-full bg-gray-800 bg-opacity-50 z-10 ${
           open ? "" : "hidden"
@@ -30,10 +27,25 @@ function Login({ open, setopen }: any) {
           <section className="">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                Sign in to your account
+                Register
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
-                
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your Name
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name"
+                    required={true}
+                  />
+                </div>
                 <div>
                   <label
                     htmlFor="email"
@@ -86,31 +98,21 @@ function Login({ open, setopen }: any) {
                       </label>
                     </div>
                   </div>
-                  <a
-                    href="#"
-                    className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-                  >
-                    Forgot password?
-                  </a>
                 </div>
                 <button
                   type="submit"
                   className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                   style={{ backgroundColor: primary_color }}
                 >
-                  Sign in
+                  Register
                 </button>
                 <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                  Don’t have an account yet?{" "}
+                  Already have an account ?{" "}
                   <a
                     href="#"
                     className="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    onClick={() => {
-                      setsignupOpen(!signupOpen);
-                      setopen(false);
-                    }}
                   >
-                    Sign up
+                    Sign In
                   </a>
                 </p>
               </form>
@@ -122,4 +124,4 @@ function Login({ open, setopen }: any) {
   );
 }
 
-export default Login;
+export default Signup;
